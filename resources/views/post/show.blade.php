afda34614f10ac5ed1d1dbd683828e48faac0615
”daisukegit config --global user.email sin2014003372@gmail.comgit config -lgit config -l$ git config --global user.name ”daisuke <sin2014003372@gmail.com>
2020-11-02 18:44:54 +0000

    デプロイテスト

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -7,18 +7,24 @@
             <div class="col-md-8 mx-auto">
                 <h2>投稿内容</h2>
                 <div class="form-group row">
-                    <label class="col-md-2" for="title">タイトル</label>
                     <div class="col-md-10">
-                        <p>{{ $post->title }}</p>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2" for="body">本文</label>
-                        <div class="col-md-10">
-                            <table class="table table-dark">
-                                <p>{{ $post->body }}</p>
-                                <a href="{{ url('post/top') }}">戻る</a></button>
-                            </table>
-                        </div>
+                        <table class="table table-dark">
+                            <thead>
+                                <tr>
+                                    <th width="10%">ID</th>
+                                    <th width="20%">タイトル</th>
+                                    <th width="20%">内容</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>{{ $post->id }}</th>
+                                    <td>{{ \Str::limit($post->title, 20) }}</td>
+                                    <td>{{ \Str::limit($post->body, 50) }}</td>
+                                </tr>
+                             </tbody>   
+                        </table>
+                        <a href="{{ url('post/top') }}"><input type="submit" class="btn btn-primary" value="戻る"></a>
                     </div>
                 </div>
             </div>
